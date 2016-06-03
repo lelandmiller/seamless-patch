bdb1172eb92e8b641346781bc2fd883a3963b554
Leland Miller <lelandmiller@gmail.com>
2016-06-03 07:26:03 -0700

    Add root pointer test

--- tests/pointer.ts
+++ tests/pointer.ts
@@ -1,2 +1,8 @@
 import test = require('tape');
 import {parse} from '../src/pointer';
+
+test('pointer.parse should parse `/` as an empty array', t => {
+    const result = parse('/');
+    t.deepEqual(result, []);
+    t.end();
+});
