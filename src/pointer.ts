a9a464f7f0c90b205b1754aba8471c510f2f36a6
Leland Miller <lelandmiller@gmail.com>
2016-06-05 09:50:38 -0700

    Add pointer parse function

--- src/pointer.ts
+++ src/pointer.ts
@@ -1,3 +1,13 @@
-export function parse(pointer: string): string[] {
-    return [];
+export function parse(pointer: string): [Error, string[]] {
+    const err = pointer.length > 0 && pointer.charAt(0) !== '/'
+        ? new Error('invalid pointer syntax')
+        : null;
+    return [err, pointer.split('/').slice(1).map(unescape)];
+}
+
+const escape1 = /~0/g;
+const escape2 = /~1/g;
+
+function unescape(token: string) {
+    return token.replace(escape2, '/').replace(escape1, '~');
 }
--- test/pointer.ts
+++ test/pointer.ts
@@ -1,8 +1,35 @@
 import test = require('tape');
 import {parse} from '../src/pointer';
 
-test('pointer.parse should parse `/` as an empty array', t => {
-    const result = parse('/');
-    t.deepEqual(result, []);
+const validCases: [string, string[]][] = [
+    ['', []],
+    ['/', ['']],
+    ['/foo/-', ['foo', '-']],
+    ['/foo/bar', ['foo', 'bar']],
+    ['/foo~0/bar~1/~0~1/~1~0/~0~0', ['foo~', 'bar/', '~/', '/~', '~~']],
+    ['/~00/~01/~10/~11', ['~0', '~1', '/0', '/1']],
+];
+
+test('parse should parse valid pointers', t => {
+    validCases.forEach(([pointerString, pointerArray]) => {
+        const [err, result] = parse(pointerString);
+        t.equal(err, null, `should not return an error for ${pointerString}`);
+        t.deepEqual(result, pointerArray, `should correctly parse ${pointerString}`);
+    });
+    t.end();
+});
+
+const invalidCases = [
+    'foo',
+    'foo/bar',
+];
+
+test('parse should return an Error for invalid pointers', t => {
+    invalidCases.forEach(pointer => {
+        const [err] = parse(pointer);
+        if (!(err instanceof Error)) {
+            t.fail(`should return an error for ${pointer}`);
+        }
+    });
     t.end();
 });
